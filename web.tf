8276e3dac7f87767cfeb4502dd6ba158a009aba1
Rodrigo Baldeón <rodrigo.baldeonj@gmail.com>
2026-04-23 09:08:04 -0500

    docs:crecion de archivo web.tf

---
+++ web.tf
@@ -0,0 +1,32 @@
+# Backend
+resource "docker_container" "api" {
+  name  = "api-${terraform.workspace}"
+  image = "lab/api"
+
+  ports {
+    internal = "3000"
+    external = var.api_port[terraform.workspace]
+  }
+}
+
+# Frontend
+resource "docker_container" "web" {
+  name  = "web-${terraform.workspace}"
+  image = "lab/web"
+
+  ports {
+    internal = "80"
+    external = var.web_port[terraform.workspace]
+  }
+}
+
+# Base de datos
+resource "docker_container" "bd" {
+  name  = "bd-${terraform.workspace}"
+  image = "postgres"
+
+  ports {
+    internal = "5432"
+    external = var.bd_port[terraform.workspace]
+  }
+}
